d78e7514c2109765c4d7d28e45533458d2506326
jcm111859242 <2232352247@qq.com>
2023-03-05 22:57:10 +0800

    first commit

---
+++ Src/Server/GameServer/GameServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SkillBridge.Message;
+using ProtoBuf;
+using System.IO;
+using Common;
+using System.Threading;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fi = new System.IO.FileInfo("log4net.xml");
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(fi);
+            Log.Init("GameServer");  //初始化日志
+            Log.Info("Game Server Init");
+
+            GameServer server = new GameServer();
+            server.Init();
+            server.Start();
+            Console.WriteLine("Game Server Running......");
+            CommandHelper.Run();  //CommandHelper里面负责日志输出内容，比如help、exit等
+            Log.Info("Game Server Exiting...");
+            server.Stop();
+            Log.Info("Game Server Exited");
+        }
+    }
+}
